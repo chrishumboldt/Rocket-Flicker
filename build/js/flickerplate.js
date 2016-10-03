3b67dc24592e19b02832ff320e7d3828d77c3808
Chris Humboldt <chrismodem@gmail.com>
2016-10-03 16:06:34 +0200

    Lots of changes.

--- build/js/flickerplate.js
+++ build/js/flickerplate.js
@@ -1,413 +1,199 @@
 /**
- * File: flickerplate.js
- * Type: Javascript component
+ * File: buil/js/flickerplate.js
+ * Type: Javascript component file
  * Author: Chris Humboldt
- */
+**/
 
-// Table of contents
-// Defaults
-// Tools
-// Touch check
-// Variables
-// Options
-// Internal functions
-// Execute
+var Flickerplate = function () {
+	// Variables
+	var defaults = {
+		selector: '.flickerplate',
+		animation: 'transform-slide',
+		arrows: true,
+		arrowsConstraint: false,
+		autoFlick: true,
+		autoFlickDelay: 10,
+		dotAlignment: 'center',
+		dots: true,
+		position: 1,
+		theme: 'light'
+	};
+	// HTML
+	var html = {
+		arrow: function (direction) {
+			var direction = direction || 'left';
+			var elmArrow = document.createElement('div');
+			elmArrow.className = 'flickerplate-arrow _' + direction;
+			return elmArrow;
+		},
+		dots: function (count) {
+			var dots = document.createElement('div');
+			var dotsUl = document.createElement('ul');
 
-// Defaults
-var $flickerplateDefault = {
-	selector: '.flickerplate',
-	animation: 'transform-slide',
-	arrows: true,
-	arrowsConstraint: false,
-	autoFlick: true,
-	autoFlickDelay: 10,
-	dotAlignment: 'center',
-	dots: true,
-	position: 1,
-	theme: 'light'
-};
+			dots.className = 'flickerplate-dots';
+			for (var i = 0, len = count; i < len; i++) {
+				var dotLi = document.createElement('li');
+				var dot = document.createElement('div');
+				dot.className = (i === 0) ? 'dot _active' : 'dot';
 
-function flickerplate($userOptions) {
-	// Tools
-	var tool = function(document) {
-		// Elements
-		var $toolEl = {
+				dotLi.appendChild(dot);
+				dotsUl.appendChild(dotLi);
+			}
+			dots.appendChild(dotsUl);
+			return dots;
+		}
+	};
+	// Webplate tools (partial)
+	var web = {
+		element: {
 			body: document.getElementsByTagName('body')[0],
 			html: document.getElementsByTagName('html')[0]
-		};
-		// HTML
-		var $toolHtml = {
-			test: false
-		};
-
-		// Functions
-		var classAdd = function($element, $class) {
-			var $crtClass = $element.className;
-			if ($crtClass.match(new RegExp('\\b' + $class + '\\b', 'g')) === null) {
-				$element.className = $crtClass === '' ? $class : $crtClass + ' ' + $class;
+		},
+		classAdd: function (element, classValue) {
+			var self = this;
+			if (self.exists(element)) {
+				if (typeof classValue === 'object') {
+					for (var i = 0, len = classValue.length; i < len; i++) {
+						self.classAddExecute(element, classValue[i]);
+					}
+				} else if (self.hasWhiteSpace(classValue)) {
+					var classes = classValue.split(' ');
+					for (var i = 0, len = classes.length; i < len; i++) {
+						self.classAddExecute(element, classes[i]);
+					}
+				} else {
+					self.classAddExecute(element, classValue);
+				}
 			}
-		};
-		var classClear = function($element) {
-			$element.removeAttribute('class');
-		};
-		var classRemove = function($element, $class) {
-			if ($element.className.indexOf($class) > -1) {
-				$element.className = $element.className.split(' ').filter(function($val) {
-					return $val != $class;
+		},
+		classAddExecute: function (element, classValue) {
+			var crtClass = element.className;
+			if (crtClass.match(new RegExp('\\b' + classValue + '\\b', 'g')) === null) {
+				element.className = crtClass === '' ? classValue : crtClass + ' ' + classValue;
+			}
+		},
+		classClear: function (element) {
+			if (this.exists(element)) {
+				element.removeAttribute('class');
+			}
+		},
+		classRemove: function (element, classValue) {
+			var self = this;
+			if (self.exists(element)) {
+				if (typeof classValue === 'object') {
+					for (var i = classValue.length - 1; i >= 0; i--) {
+						self.classRemoveExecute(element, classValue[i]);
+					}
+				} else if (self.hasWhiteSpace(classValue)) {
+					var classes = classValue.split(' ');
+					for (var i = 0, len = classes.length; i < len; i++) {
+						self.classRemoveExecute(element, classes[i]);
+					}
+				} else {
+					self.classRemoveExecute(element, classValue);
+				}
+			}
+		},
+		classRemoveExecute: function (element, classValue) {
+			if (element.className.indexOf(classValue) > -1) {
+				element.className = element.className.split(' ').filter(function (val) {
+					return val != classValue;
 				}).toString().replace(/,/g, ' ');
-				if ($element.className === '') {
-					classClear($element);
+				if (element.className === '') {
+					this.classClear(element);
 				}
 			}
-		};
-		var exists = function($element) {
-			if ($element === null || typeof($element) === undefined) {
-				return false;
-			} else {
-				return true;
-			}
-		};
-		var getIndex = function($node) {
-			return [].indexOf.call($node.parentNode.children, $node);
-		};
-		var hasClass = function($element, $class) {
-			return (' ' + $element.className + ' ').indexOf(' ' + $class + ' ') > -1;
-		};
-		var isTouch = function() {
+		},
+		exists: function (check) {
+			return (check === null || check === false || typeof (check) == 'undefined') ? false : true;
+		},
+		hasClass: function (element, classValue) {
+			return (' ' + element.className + ' ').indexOf(' ' + classValue + ' ') > -1;
+		},
+		hasWhiteSpace: function (check) {
+			return /\s/.test(check);
+		},
+		isTouch: function () {
 			return 'ontouchstart' in window || 'onmsgesturechange' in window;
-		};
-		var remove = function($selector) {
-			if ($selector.charAt(0) === '#') {
-				var $element = document.getElementById($selector.substring(1));
-				if ($element !== null) {
-					$element.parentNode.removeChild($element);
-				}
-			} else if ($selector.charAt(0) === '.') {
-				var $elements = document.querySelectorAll($selector);
-				for (var $i = $elements.length - 1; $i >= 0; $i--) {
-					if ($elements[$i] !== null) {
-						$elements[$i].parentNode.removeChild($element);
+		},
+		log: function (text) {
+			if (window && window.console) {
+				console.log(text);
+			}
+		}
+	};
+	// Functions
+	var applyAllFlickers = function (options) {
+		var allflickers = document.querySelectorAll(options.selector);
+		if (allflickers.length > 0) {
+			for (var i = 0, len = allflickers.length; i < len; i++) {
+				flicker.setup(allflickers[i], options);
+			}
+		}
+	};
+	var flicker = {
+		move: function (flicker, options) {},
+		navigate: function (flicker, options) {},
+		setup: function (flicker, options) {
+			// Variables
+			var flicks = flicker.querySelectorAll('li');
+			var flickerList = flicker.querySelector('ul');
+			// Basic setup
+			web.classAdd(flicker, ['flickerplate', '_t-' + options.theme, '_a-' + options.animation]);
+			web.classAdd(flickerList, 'flicks');
+			// Each flick
+			options.flicksTotal = flicks.length;
+			if (flicks.length > 0) {
+				for (var i = 0, len = flicks.length; i < len; i++) {
+					var background = flicks[i].getAttribute('data-background') || false;
+					if (background) {
+						flicks[i].style.backgroundImage = 'url(' + background + ')';
 					}
 				}
 			}
+			// Arrow
+			if (options.animation !== 'scroller-slide' && options.arrows) {
+				flicker.insertBefore(html.arrow('left'), flickerList);
+				flicker.insertBefore(html.arrow('right'), flickerList);
+			}
+			// Dots
+			if (options.animation !== 'scroller-slide' && options.dots) {
+				flicker.insertBefore(html.dots(options.flicksTotal), flickerList);
+			}
+
+			this.move(flicker, options);
+			this.navigate(flicker, options);
+		}
+	};
+	var setup = function () {
+		if (!web.isTouch()) {
+			web.classAdd(web.element.html, 'flickerplate-no-touch');
+		}
+	};
+	// Initialize
+	var init = function (userOptions) {
+		var userOptions = userOptions || false;
+		var options = {
+			selector: userOptions.selector || defaults.selector,
+			animation: userOptions.animation || defaults.animation,
+			arrows: (typeof userOptions.arrows !== 'undefined') ? userOptions.arrows : defaults.arrows,
+			arrowsConstraint: (typeof userOptions.arrowsConstraint !== 'undefined') ? userOptions.arrowsConstraint : defaults.arrowsConstraint,
+			autoFlick: (typeof userOptions.autoFlick !== 'undefined') ? userOptions.autoFlick : defaults.autoFlick,
+			autoFlickDelay: userOptions.autoFlickDelay || defaults.autoFlickDelay,
+			dotAlignment: userOptions.dotAlignment || defaults.dotAlignment,
+			dots: (typeof userOptions.dots !== 'undefined') ? userOptions.dots : defaults.dots,
+			position: userOptions.position || defaults.position,
+			theme: userOptions.theme || defaults.theme,
+			flicksTotal: 0
 		};
-		var wrapInner = function($element, $tag, $className) {
-			if (typeof $tag === 'string') {
-				$tag = document.createElement($tag);
-			}
-			if ($className !== undefined) {
-				var $div = $element.appendChild($tag).setAttribute('class', $className);
-			} else {
-				var $div = $element.appendChild($tag);
-			}
-			while ($element.firstChild !== $tag) {
-				$tag.appendChild($element.firstChild);
-			}
-		};
-
-		return {
-			classAdd: classAdd,
-			classClear: classClear,
-			classRemove: classRemove,
-			element: $toolEl,
-			exists: exists,
-			getIndex: getIndex,
-			hasClass: hasClass,
-			html: $toolHtml,
-			isTouch: isTouch,
-			remove: remove,
-			wrapInner: wrapInner
-		}
-	}(document);
-
-	var $selector = ($userOptions && $userOptions.selector) ? $userOptions.selector : $flickerplateDefault.selector;
-	var $selectorType = $selector.charAt(0).toString();
-	if ($selectorType === '#' && $selector.indexOf('.') < 0) {
-		new flickerplateComponent(document.getElementById($selector.substring(1)), $userOptions, tool);
-	} else {
-		var $elements = document.querySelectorAll($selector);
-		for (var $i = 0; $i < $elements.length; $i++) {
-			new flickerplateComponent($elements[$i], $userOptions, tool);
-		}
-	}
-};
-
-function flickerplateComponent($element, $userOptions, tool) {
-	// Touch check
-	if (!tool.isTouch() && !tool.hasClass(tool.element.html, 'flickerplate-no-touch')) {
-		tool.classAdd(tool.element.html, 'flickerplate-no-touch');
-	}
-
-	// Variables
-	var $self = this;
-	var $autoFlickWatch;
-	var $flickCount = 0;
-	var $flickerMoving = false;
-	var $transitionEventListner = "transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd";
-
-	var $lastPosXLeft = 0;
-	var $lastPosXPercent = 0;
-	var $panCSS = 'translate3d(0, 0, 0)';
-	var $panThreshold = 100;
-
-	// Options
-	$userOptions = $userOptions || false;
-	$self.options = {
-		selector: $userOptions.selector || $flickerplateDefault.selector,
-		animation: $userOptions.animation || $flickerplateDefault.animation,
-		arrows: (typeof $userOptions.arrows !== 'undefined') ? $userOptions.arrows : $flickerplateDefault.arrows,
-		arrowsConstraint: (typeof $userOptions.arrowsConstraint !== 'undefined') ? $userOptions.arrowsConstraint : $flickerplateDefault.arrowsConstraint,
-		autoFlick: (typeof $userOptions.autoFlick !== 'undefined') ? $userOptions.autoFlick : $flickerplateDefault.autoFlick,
-		autoFlickDelay: $userOptions.autoFlickDelay || $flickerplateDefault.autoFlickDelay,
-		dotAlignment: $userOptions.dotAlignment || $flickerplateDefault.dotAlignment,
-		dots: (typeof $userOptions.dots !== 'undefined') ? $userOptions.dots : $flickerplateDefault.dots,
-		position: $userOptions.position || $flickerplateDefault.position,
-		theme: $userOptions.theme || $flickerplateDefault.theme
+		applyAllFlickers(options);
 	};
-
-	// Internal functions
-	function flickerAttachArrows() {
-		if ($self.options.animation != 'scroller-slide' && $self.options.arrows == true) {
-			var $arrowLeft = document.createElement('div');
-			var $arrowRight = document.createElement('div');
-
-			$arrowLeft.className = 'arrow-navigation left';
-			$arrowRight.className = 'arrow-navigation right';
-
-			$element.insertBefore($arrowLeft, $element.querySelector('.flicks'));
-			$element.insertBefore($arrowRight, $element.querySelector('.flicks'));
-
-			// Click event
-			var $arrowNavigationElements = $element.querySelectorAll('.arrow-navigation');
-			$arrowLeft.onclick = function() {
-				if (--$self.options.position < 1) {
-					$self.options.position = $self.options.arrowsConstraint ? 1 : $flickCount;
-				}
-				flickerMove();
-				flickerAutoReset();
-			};
-			$arrowRight.onclick = function() {
-				if (++$self.options.position > $flickCount) {
-					$self.options.position = $self.options.arrowsConstraint ? $flickCount : 1;
-				}
-				flickerMove();
-				flickerAutoReset();
-			};
-		}
+	// Return
+	return {
+		defaults: defaults,
+		init: init,
+		setup: setup
 	};
+}();
 
-	function flickerAttachDots() {
-		if ($self.options.animation !== 'scroller-slide' && $self.options.dots === true) {
-			var $dots = document.createElement('div');
-			var $dotsUL = document.createElement('ul');
-			$dots.className = 'dot-navigation ' + $self.options.dotAlignment;
-
-			for (var $i = $flickCount - 1; $i >= 0; $i--) {
-				var $dotLI = document.createElement('li');
-				var $dot = document.createElement('div');
-				$dot.className = ($i === $flickCount - 1) ? 'dot active' : 'dot';
-
-				$dotLI.appendChild($dot);
-				$dotsUL.appendChild($dotLI);
-			};
-
-			$dots.appendChild($dotsUL);
-			$element.insertBefore($dots, $element.querySelectorAll('.flicks')[0]);
-
-			// Events
-			var $dotElements = $element.querySelectorAll('.dot-navigation li');
-			for (var $i = $dotElements.length - 1; $i >= 0; $i--) {
-				$dotElements[$i].onclick = function() {
-					$self.options.position = tool.getIndex(this) + 1;
-					tool.classRemove($element.querySelector('.dot.active'), 'active');
-					tool.classAdd(this.querySelector('.dot'), 'active');
-					flickerMove();
-					flickerAutoReset();
-				};
-			};
-		}
-	};
-
-	function flickerAutoFlick() {
-		if (++$self.options.position > $flickCount) {
-			$self.options.position = 1;
-		}
-		flickerMove();
-	};
-
-	function flickerAutoReset() {
-		flickerAutoStop();
-		flickerAutoStart();
-	};
-
-	function flickerAutoStart() {
-		if ($self.options.autoFlick === true) {
-			$autoFlickWatch = setInterval(flickerAutoFlick, $self.options.autoFlickDelay * 1000);
-		}
-	};
-
-	function flickerAutoStop() {
-		if ($self.options.autoFlick === true) {
-			$autoFlickWatch = clearInterval($autoFlickWatch);
-		}
-	};
-
-	function flickerMove($firstCheck) {
-		$firstCheck = $firstCheck || false;
-		var $flicks = $element.querySelector('ul.flicks');
-		var $movePosition = $self.options.position - 1;
-		$element.setAttribute('data-flickerplate-position', $self.options.position);
-
-		switch ($self.options.animation) {
-			case 'transform-slide':
-				var $translate3D = 'translate3d(-' + $movePosition + '%, 0, 0)';
-				$flicks.setAttribute('style', '-webkit-transform:' + $translate3D + ';-o-transform:' + $translate3D + ';-moz-transform:' + $translate3D + ';transform:' + $translate3D);
-				$lastPosXPercent = -($movePosition);
-				break;
-			case 'transition-fade':
-				var $allFlicks = $element.querySelectorAll('li');
-				for (var $i = $allFlicks.length - 1; $i >= 0; $i--)(function($i) {
-					tool.classRemove($allFlicks[$i], 'active');
-				})($i);
-				tool.classAdd($flicks.querySelector('li:nth-child(' + $self.options.position + ')'), 'active');
-				break;
-			case 'transition-slide':
-				$flicks.style.left = '-' + $movePosition + '00%';
-				$lastPosXLeft = -($movePosition + '00');
-				break;
-		}
-
-		if ($self.options.dots === true && $firstCheck === false) {
-			tool.classRemove($element.querySelector('.dot.active'), 'active');
-			tool.classAdd($element.querySelector('.dot-navigation li:nth-child(' + $self.options.position + ') .dot'), 'active');
-		}
-	};
-
-	function flickerHammer() {
-		if (typeof Hammer === 'function') {
-			if ($self.options.animation === 'transform-slide' || $self.options.animation === 'transition-slide') {
-				// Interaction
-				var $hammerTime = new Hammer($element.querySelector('ul.flicks'));
-				$hammerTime.on('panleft panright', function(event) {
-					flickerPan(event);
-				});
-				$hammerTime.on('panend', function(event) {
-					flickerPanEnd(event);
-				});
-			} else if ($self.options.animation === 'transition-fade') {
-				var $hammerTime = new Hammer($element.querySelector('ul.flicks'));
-				$hammerTime.on('swipeleft swiperight', function(event) {
-					flickerSwipe(event);
-				});
-			}
-		}
-	};
-
-	function flickerPan(event) {
-		flickerAutoStop();
-		var $flickerWidth = $element.clientWidth;
-		var $flicks = $element.querySelector('ul.flicks');
-		tool.classRemove($element, 'animate-' + $self.options.animation);
-
-		switch ($self.options.animation) {
-			case 'transform-slide':
-				if (tool.isTouch()) {
-					$posX = (Math.round((event.deltaX / $flickerWidth) * 1000) / 1000) + $lastPosXPercent;
-				} else {
-					$posX = (Math.round((event.deltaX / $flickerWidth) * 10) / 10) + $lastPosXPercent;
-				}
-
-				// Check constraints
-				if ($self.options.position == 1 && $posX > 0) {
-					$posX = 0;
-				} else if (($self.options.position == $flickCount) && ($posX < -($flickCount - 1))) {
-					$posX = -($flickCount - 1)
-				}
-
-				// Move
-				$panCSS = 'translate3d(' + $posX + '%, 0, 0)';
-				$flicks.setAttribute('style', '-webkit-transform:' + $panCSS + ';-o-transform:' + $panCSS + ';-moz-transform:' + $panCSS + ';transform:' + $panCSS);
-				break;
-			case 'transition-slide':
-				$posX = Math.round((event.deltaX / $flickerWidth) * 100) + $lastPosXLeft;
-
-				// Check constraint
-				if ($self.options.position == 1 && $posX > 0) {
-					$posX = 0;
-				} else if (($self.options.position == $flickCount) && ($posX < -($flickCount - 1) * 100)) {
-					$posX = -(($flickCount - 1) * 100);
-				}
-
-				// Move
-				$flicks.style.left = $posX + '%';
-				break;
-		}
-	};
-
-	function flickerPanEnd(event) {
-		var $endPosX = event.deltaX;
-		tool.classAdd($element, 'animate-' + $self.options.animation);
-
-		if (($endPosX < -$panThreshold) && ($self.options.position < $flickCount)) {
-			$self.options.position++;
-		} else if (($endPosX > $panThreshold) && ($self.options.position > 1)) {
-			$self.options.position--;
-		}
-
-		setTimeout(function() {
-			flickerMove();
-		}, 10);
-		flickerAutoStart();
-	};
-
-	function flickerSetup() {
-		tool.classAdd($element, 'flickerplate theme-' + $self.options.theme + ' animate-' + $self.options.animation);
-		tool.classAdd($element.getElementsByTagName('ul')[0], 'flicks');
-		$element.setAttribute('data-flickerplate-position', $self.options.position);
-
-		flickerMove(true);
-
-		// Each flick
-		var $flicks = $element.querySelectorAll('ul.flicks > li');
-		for (var $i = $flicks.length - 1; $i >= 0; $i--) {
-			$flickCount++;
-			tool.wrapInner($flicks[$i], 'div', 'flick-inner');
-			tool.wrapInner($flicks[$i].querySelectorAll('.flick-inner')[0], 'div', 'flick-content');
-
-			var $background = $flicks[$i].getAttribute('data-background') || false;
-			if ($background !== false) {
-				$flicks[$i].style.backgroundImage = 'url(' + $background + ')';
-			}
-		}
-
-		// Kill the animation
-		if ($self.options.animation != 'scroller-slide' && $self.options.animation != 'jquery-slide' && $self.options.animation != 'jquery-fade') {
-			$element.addEventListener($transitionEventListner, function() {
-				$flickerMoving = false;
-			});
-		}
-	};
-
-	function flickerSwipe(event) {
-		if (event.type == 'swipeleft') {
-			if (++$self.options.position > $flickCount) {
-				$self.options.position = $self.options.arrowsConstraint ? $flickCount : 1;
-			}
-		} else if (event.type == 'swiperight') {
-			if (--$self.options.position < 1) {
-				$self.options.position = $self.options.arrowsConstraint ? 1 : $flickCount;
-			}
-		}
-		flickerMove();
-		flickerAutoReset();
-	};
-
-	// Execute
-	flickerSetup();
-	flickerAttachArrows();
-	flickerAttachDots();
-	flickerAutoStart();
-	if (tool.isTouch()) {
-		flickerHammer();
-	}
-}
+Flickerplate.setup();
