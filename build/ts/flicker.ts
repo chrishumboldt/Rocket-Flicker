efc2b9cf710c0f97b63cf8aefa7f0ca695d3dad1
Chris Humboldt <chrismodem@gmail.com>
2017-02-15 11:56:41 +0200

    Changes coming.

---
+++ build/ts/flicker.ts
@@ -0,0 +1,18 @@
+/*
+Author: Chris Humboldt
+*/
+
+// Rocket module extension
+Rocket.defaults.flicker = {
+   target: '.flicker',
+   animation: 'transform-slide',
+   arrows: true,
+   arrowsConstraint: false,
+   autoFlick: true,
+   autoFlickDelay: 10,
+   dotAlignment: 'center',
+   dots: true,
+   position: 1
+};
+
+Rocket.log('yay');
